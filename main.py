c057321d9c7be38aba3718b7791805499fa2888f
pchs20 <pauduranmanzano@gmail.com>
2025-03-09 18:24:21 +0100

    First query: hello world

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
+from graphene import ObjectType, String, Schema
+
+
+class Query(ObjectType):
+    hello = String(name=String(default_value='world'))
+
+    @staticmethod
+    def resolve_hello(root, info, name):
+        return f'Hello {name}'
+
+
+schema = Schema(query=Query)
+
+gql = """
+{
+    hello(name: "GraphQL")
+}
+"""
+
 
 if __name__ == '__main__':
-    pass
+    result = schema.execute(gql)
+    print(result)
