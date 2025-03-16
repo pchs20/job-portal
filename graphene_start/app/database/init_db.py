14cfc1dab415d42b3e8e61d9638f617020534695
pchs20 <pauduranmanzano@gmail.com>
2025-03-16 20:17:46 +0100

    Remove id's from inital data

--- graphene_start/app/database/init_db.py
+++ graphene_start/app/database/init_db.py
@@ -5,13 +5,11 @@ from app.models import Employer, Job
 
 employers_data = [
     {
-        'id': 1,
         'name': 'MetaTechA',
         'contact_email': 'contact@company-a.com',
         'industry': 'Tech',
     },
     {
-        'id': 2,
         'name': 'MoneySoftB',
         'contact_email': 'contact@company-b.com',
         'industry': 'Finance',
@@ -20,25 +18,21 @@ employers_data = [
 
 jobs_data = [
     {
-        'id': 1,
         'title': 'Software Engineer',
         'description': 'Develop web applications',
         'employer_id': 1,
     },
     {
-        'id': 2,
         'title': 'Data Analyst',
         'description': 'Analyze data and create reports',
         'employer_id': 1,
     },
     {
-        'id': 3,
         'title': 'Accountant',
         'description': 'Manage financial records',
         'employer_id': 2,
     },
     {
-        'id': 4,
         'title': 'Manager',
         'description': 'Manage people who manage records',
         'employer_id': 2,
