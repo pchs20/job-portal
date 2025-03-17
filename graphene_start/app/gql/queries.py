04b8d680fb0ddefee0b14e33a0527192b0b7caa9
pchs20 <pauduranmanzano@gmail.com>
2025-03-17 10:17:22 +0100

    Get job by id query

--- graphene_start/app/gql/queries.py
+++ graphene_start/app/gql/queries.py
@@ -1,4 +1,4 @@
-from graphene import List, ObjectType
+from graphene import Field, Int, List, ObjectType
 
 from app.deps import get_repository
 from app.gql import EmployerObject, JobObject
@@ -7,12 +7,21 @@ from app.respositories import EmployerRepository, JobRepository
 
 class Query(ObjectType):
     jobs = List(JobObject)
+    job = Field(JobObject, id=Int(required=True))
     employers = List(EmployerObject)
 
     @staticmethod
     async def resolve_jobs(root, info):
         jobs_repo = await get_repository(JobRepository)
         return await jobs_repo.get_multi()
+
+    @staticmethod
+    async def resolve_job(root, info, id):
+        jobs_repo = await get_repository(JobRepository)
+        job = await jobs_repo.get(id)
+        if job is None:
+            raise Exception(f'Job with id {id} not found')
+        return job
 
     @staticmethod
     async def resolve_employers(root, info):
