7373a184858ffa26e67f942bbd574b27844716ef
pchs20 <pauduranmanzano@gmail.com>
2025-03-17 10:11:39 +0100

    Update job mutation

--- graphene_start/app/gql/mutations.py
+++ graphene_start/app/gql/mutations.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from graphene import Field, Int, Mutation, ObjectType, String
 
 from app.deps import get_repository
@@ -22,5 +24,38 @@ class AddJob(Mutation):
         return AddJob(job=job)
 
 
+class UpdateJob(Mutation):
+    class Arguments:
+        job_id = Int(required=True)
+        title = String(required=False)
+        description = String(required=False)
+        employer_id = Int(required=False)
+
+    job = Field(lambda: JobObject)
+
+    @staticmethod
+    async def mutate(
+        root,
+        info,
+        job_id,
+        title=None,
+        description=None,
+        employer_id=None,
+    ):
+        jobs_repo = await get_repository(JobRepository)
+        fields_to_update: dict[str, Any] = {}
+        if title is not None:
+            fields_to_update['title'] = title
+        if description is not None:
+            fields_to_update['description'] = description
+        if employer_id is not None:
+            fields_to_update['employer_id'] = employer_id
+        job = await jobs_repo.update(job_id, fields_to_update)
+        if job is None:
+            raise Exception(f'Job with id {job_id} not found')
+        return UpdateJob(job=job)
+
+
 class Mutation(ObjectType):  # type: ignore
     add_job = AddJob.Field()
+    update_job = UpdateJob.Field()
--- graphene_start/app/respositories/jobs.py
+++ graphene_start/app/respositories/jobs.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from sqlalchemy import select
 from sqlalchemy.orm import selectinload
 
@@ -6,6 +8,12 @@ from app.respositories import BaseRepository
 
 
 class JobRepository(BaseRepository):
+    async def get(self, job_id: int) -> Job | None:
+        query = select(Job).where(Job.id == job_id)
+        query = query.options(selectinload(Job.employer))
+        result = await self._db.execute(query)
+        return result.scalars().one_or_none()
+
     async def get_multi(self) -> list[Job]:
         query = select(Job)
         query = query.options(selectinload(Job.employer))
@@ -22,3 +30,12 @@ class JobRepository(BaseRepository):
         await self.commit()
         await self._db.refresh(new_job)
         return new_job
+
+    async def update(self, job_id: int, job_in: dict[str, Any]) -> Job | None:
+        job = await self.get(job_id)
+        if job is None:
+            return None
+        self.update_model(model=job, update=job_in)
+        await self.commit()
+        await self._db.refresh(job)
+        return job
