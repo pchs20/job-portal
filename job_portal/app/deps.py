8cb35bc7eface648fb7bd781debc05e085f0e7d4
pchs20 <pauduranmanzano@gmail.com>
2025-04-19 20:41:36 +0200

    Get authenticated user decoding JWT token

--- job_portal/app/deps.py
+++ job_portal/app/deps.py
@@ -1,8 +1,11 @@
 from typing import Type, TypeVar
 
-from app.database import async_session
-from app.respositories import BaseRepository
+from graphql import GraphQLError
 
+from app.database import async_session
+from app.models import User
+from app.respositories import BaseRepository, UserRepository
+from app.security import decode_access_token
 
 Repository = TypeVar('Repository', bound=BaseRepository)
 
@@ -11,3 +14,21 @@ async def get_repository(repo_type: Type[Repository]) -> Repository:
     """Factory function to create repository instances."""
     async with async_session() as session:
         return repo_type(session)
+
+
+async def get_authenticated_user(context) -> User:
+    request_obj = context.get('request')
+    auth_header = request_obj.headers.get('Authorization')
+    if auth_header is None:
+        raise GraphQLError('Authorization header not provided')
+
+    # Expected format for Authentication header is: 'Bearer <token>'
+    token = auth_header.split(' ')[1]
+    payload = decode_access_token(token)
+
+    email = payload.get('sub')
+    users_repo = await get_repository(UserRepository)
+    user = await users_repo.get_by_email(email=email)
+    if user is None:
+        raise GraphQLError('Could not authenticate user')
+    return user
--- job_portal/app/security.py
+++ job_portal/app/security.py
@@ -1,7 +1,8 @@
 from datetime import datetime, timedelta, timezone
-
+from typing import Any
 
 import jwt
+from graphql import GraphQLError
 from passlib.context import CryptContext
 
 from app.settings import SecuritySettings
@@ -20,7 +21,7 @@ def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
 
 
-def create_access_token(email: str, expiration_delta: timedelta | None = None):
+def create_access_token(email: str, expiration_delta: timedelta | None = None) -> str:
     if not expiration_delta:
         expiration_delta = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     expire = datetime.now(tz=timezone.utc) + expiration_delta
@@ -34,3 +35,17 @@ def create_access_token(email: str, expiration_delta: timedelta | None = None):
         algorithm=settings.TOKEN_ENCRYPTION_ALGORITHM,
     )
     return encoded_jwt
+
+
+def decode_access_token(token: str) -> dict[str, Any]:
+    try:
+        payload = jwt.decode(
+            token,
+            settings.SECRET_KEY,
+            algorithms=[settings.TOKEN_ENCRYPTION_ALGORITHM],
+        )
+    except jwt.ExpiredSignatureError:
+        raise GraphQLError('Token has expired')
+    except jwt.InvalidTokenError:
+        raise GraphQLError('Invalid token')
+    return payload
